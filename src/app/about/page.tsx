8d2cc0178cd621b44b062ea36089eb6d8ec8ce94
saimun-orangetoolz <saimun@orangetoolz.com>
2025-05-12 23:36:45 +0600

    added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,7 @@ const page = () => {
     <div className='bg-blue-500'>
       <h1 className='text-3xl '>This is about page</h1>
       <h1 className='text-3xl '>This is update page</h1>
+      <h1 className='text-3xl text-green-500'>saimun</h1>
     </div>
   )
 }
